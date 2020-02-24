1063ac0fe27e191a61f6fe0e349b55f55aa7c9b0
Nikita <nikuuu66@gmail.com>
2020-02-24 15:38:02 +0200

    Add new view for new elements

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,7 @@ class ItemsController < ApplicationController
 
   # /items/new GET
   def new
+  	@item = Item.new
   end
 
   # /items/1/edit GET
@@ -22,7 +23,11 @@ class ItemsController < ApplicationController
   # /items POST
   def create
     @item = Item.create(params[:item])
-    render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
+    if @item.errors.empty?
+    	redirect_to item_path(@item)
+    else
+    	render "new"
+    end
   end
 
   # /items/1 PUT
